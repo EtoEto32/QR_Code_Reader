e4522f2d3057cb692a2a4ca97cf2c0b4414ada64
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2025-02-13 18:59:50 +0900

    後は、FetchAPIでQRコードのデータを送る処理が必要

--- script.js
+++ script.js
@@ -1,17 +1,74 @@
-const video=document.getElementById('video');
-let contentWidth;
-let contentHeght;
+//カメラ映像の表示
+const video=document.getElementById("video");
+let contentWidth,contentHeight;
 
-const media=navigator.mediaDevices.getUserMedia({audio:false,video:{width:640,height:480}})
+const media=navigator.mediaDevices.getUserMedia({
+  audio:false,video:{width:640,height:480}})
   .then((stream)=>{
     video.srcObject=stream;
     video.onloadeddata=()=>{
-      video.onplay();
+      video.play();
       contentWidth=video.clientWidth;
       contentHeight=video.clientHeight;
-      canvasUpdate();
-      checkImage();
+      canvasUpdate();//以降記述
+      checkImage();//以降記述
     }
-  }).catch((e)=>{
-    console.log(e);
-});
+
+  });
+
+//webカメラの映像をcanvasに描画する処理
+
+const cvs=document.getElementById('camera-canvas');
+const ctx=cvs.getContext("2d");
+
+const canvasUpdate=()=>{
+  cvs.width=contentWidth;
+  cvs.height=contentHeight;
+  ctx.drawImage(video,0,0,contentWidth,contentHeight);
+  requestAnimationFrame(canvasUpdate);
+}
+
+// QRコードの検出
+const rectCvs=document.getElementById('rect-canvas');
+const rectCtx=rectCvs.getContext("2d");
+
+const checkImage=()=>{
+  // imageDataの取得
+  const imageData=ctx.getImageData(0,0,contentWidth,contentHeight);
+  // jsQRに渡す
+  console.log(imageData);
+  const code=jsQR(imageData.data,contentWidth,contentHeight);
+
+  // 検出結果に応じて処理を実施
+  if(code){
+    console.log("QRコードの中身：",code);
+    drawRect(code.location);
+  }else{
+    console.log("QRコードが見つかりません...:",code);
+    rectCtx.clearRect(0,0,contentWidth,contentHeight);
+  }
+  setTimeout(()=>{checkImage()},500);
+}
+// 図形の描画処理
+const drawRect=(location)=>{
+  rectCvs.width=contentWidth;
+  rectCvs.height=contentHeight;
+  drawLine(location.topLeftCorner, location.topRightCorner);
+  drawLine(location.topRightCorner, location.bottomRightCorner);
+  drawLine(location.bottomLeftCorner, location.bottomRightCorner);
+  drawLine(location.bottomLeftCorner, location.topLeftCorner);
+}
+
+// 線の描画処理
+const drawLine=(begin,end)=>{
+  rectCtx.lineWidth=4;
+  rectCtx.strokeStyle="red";
+  rectCtx.beginPath();
+  rectCtx.moveTo(begin.x,begin.y);
+  rectCtx.lineTo(end.x,end.y);
+  rectCtx.stroke();
+}
+
+
+
+// Fetch APIを使ってQRコードの内容を特定のURLに送信する処理
